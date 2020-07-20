d60287d5539fb41924573d2bcdcb51777b596762
LarryZheng <351220018@qq.com>
2020-07-20 14:24:26 +0800

    upd: 更新

--- src/interface/IErrorHandle.ts
+++ src/interface/IErrorHandle.ts
@@ -1,6 +1,9 @@
 import {ErrorMessage} from './ErrorMessage';
+import {ErrorDetail} from './ErrorDetail';
 
 export interface IErrorHandle {
   getLineError(errorStack: string): Array<string>;
   handle(errorStact: string): ErrorMessage;
+  getMessage(errorDetailStr: string): string;
+  getErrorDetail(errorDetailStr: string): ErrorDetail;
 }
